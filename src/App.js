13f7209d21ef54605d332b3eee5fe6b4d6cc92ed
wyndhams <wyn.roy@hotmail.co.uk>
2023-01-05 17:09:04 +0000

    single workout page api call working

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
 import Navbar from './components/Navbar';
 import WorkoutDirectory from './components/WorkoutDirectory';
-import Workouts from './components/Workouts';
+import AllWorkouts from './components/AllWorkouts';
+import SingleWorkout from './components/SingleWorkout';
 import CreateWorkout from './components/CreateWorkout';
 import WorkoutLog from './components/WorkoutLog';
 import Login from './components/Login';
@@ -48,9 +49,10 @@ function App() {
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/workout-directory' element={<WorkoutDirectory />} />
-          <Route path='/workouts' element={<Workouts />} />
+          <Route path='/workouts' element={<AllWorkouts />} />
           <Route path='/workouts/create' element={<CreateWorkout />} />
           <Route path='workout-log' element={<WorkoutLog />} />
+          <Route path='workouts/:id' element={<SingleWorkout />} />
           <Route path='/login' element={<Login />} />
           <Route path='/register' element={<Register />} />
           <Route path='/account' element={<Account />} />

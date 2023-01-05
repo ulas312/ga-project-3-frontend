5b62c0382334d1b0d217ab0e56e9ec96a4bd301b
312 <a312@312s-MBP.broadband>
2023-01-05 13:37:00 +0000

    return for acount page

---
+++ src/components/Account.js
@@ -0,0 +1,9 @@
+const Account = () => (
+  <section className='Home'>
+    <div className='welcome'>
+      <h1>PROFILE PAGE</h1>
+    </div>
+  </section>
+);
+
+export default Account;
---
+++ src/components/WorkoutLog.js
@@ -0,0 +1,9 @@
+const WorkoutLog = () => (
+  <section className='Home'>
+    <div className='welcome'>
+      <h1>WORKOUT LOG</h1>
+    </div>
+  </section>
+);
+
+export default WorkoutLog;

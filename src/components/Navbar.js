ce22838180673c4e4db403348f3e0cd0b874b742
312 <a312@312s-MBP.broadband>
2023-01-05 13:27:05 +0000

    rerturn for nav

---
+++ src/components/Navbar.js
@@ -0,0 +1,22 @@
+import { fallDown as Menu } from 'react-burger-menu';
+
+const Navbar = () => {
+  return (
+    <Menu>
+      <a id='home' className='menu-item' href='/'>
+        Home
+      </a>
+      <a id='about' className='menu-item' href='/about'>
+        About
+      </a>
+      <a id='contact' className='menu-item' href='/contact'>
+        Contact
+      </a>
+      <a onClick={this.showSettings} className='menu-item--small' href=''>
+        Settings
+      </a>
+    </Menu>
+  );
+};
+
+export default Navbar;

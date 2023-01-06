89f5dee57761fd0e4d7d72f3b1882d163169fa41
wyndhams <wyn.roy@hotmail.co.uk>
2023-01-06 10:34:00 +0000

    workout directory added to

--- src/components/WorkoutDirectory.js
+++ src/components/WorkoutDirectory.js
@@ -1,14 +1,32 @@
 import { Link, useNavigate } from 'react-router-dom';
-import { Typography } from '@mui/material';
+import { Button, Typography, CardActionArea } from '@mui/material';
 
 const WorkoutDirectory = () => {
   const navigate = useNavigate();
+
   return (
-    <Link to='/'>
-      <Typography variant='h6' color='inherit' component='div' sx={{ mr: 2 }}>
-        Chest
-      </Typography>
-    </Link>
+    <>
+      <Link to='/'>
+        <Typography variant='h6' color='inherit' component='div' sx={{ mr: 2 }}>
+          Home
+        </Typography>
+      </Link>
+      <CardActionArea>
+        <Button color='inherit'>Chest, Shoulders, Triceps</Button>
+      </CardActionArea>
+      <CardActionArea>
+        <Button color='inherit'>Back & Biceps</Button>
+      </CardActionArea>
+      <CardActionArea>
+        <Button color='inherit'>Legs & Abs</Button>
+      </CardActionArea>
+      <CardActionArea>
+        <Button color='inherit'>Full Body</Button>
+      </CardActionArea>
+      <CardActionArea>
+        <Button color='inherit'>Submit</Button>
+      </CardActionArea>
+    </>
   );
 };
 

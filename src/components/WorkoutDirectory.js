c7c98f2b64029c73fb73c5fb03dcd78da1c158e7
wyndhams <wyn.roy@hotmail.co.uk>
2023-01-06 17:51:58 +0000

    workout directory amended to display muscle card content.

--- src/components/WorkoutDirectory.js
+++ src/components/WorkoutDirectory.js
@@ -9,17 +9,15 @@ const WorkoutDirectory = () => {
   const [workouts, setWorkouts] = useState(null);
   const goToSelectedWorkouts = () =>
     navigate({
-      pathname: API.ENDPOINTS.workoutsByMuscleGroup,
-      search: `?${createSearchParams(
-        workouts.map((workout) => workout.workout.id)
-      )}`,
+      pathname: API.ENDPOINTS.workoutDirectory,
+      search: `?${createSearchParams(workouts.workout)}`,
     });
 
   useEffect(() => {
-    API.GET(API.ENDPOINTS.workoutsByMuscleGroup)
+    API.GET(API.ENDPOINTS.workoutDirectory)
       .then(({ data }) => {
         setWorkouts(data);
-        console.log(API.ENDPOINTS.workoutsByMuscleGroup);
+        console.log(API.ENDPOINTS.workoutDirectory);
         console.log(data);
       })
       .catch(({ message, response }) => {
@@ -30,7 +28,6 @@ const WorkoutDirectory = () => {
   if (!workouts) {
     return null;
   }
-
 
   // const [selectedWorkouts, setSelectedWorkouts] = useState([]);
 
@@ -45,14 +42,19 @@ const WorkoutDirectory = () => {
         <Button color='inherit'>Chest, Shoulders, Triceps</Button>
       </CardActionArea>
       } */}
-      <Container maxWidth='lg' sx={{ display: 'flex' }} className='Workout'>
-        {workouts.map((workout) => (
-          <Grid item xs={4} key={workout._id}>
-            <MuscleCard name={workout.name} image={workout.image} />
-          </Grid>
-        ))}
+      <Container maxWidth='lg'>
+        <Grid container spacing={2}>
+          {workouts?.map((workout) => (
+            <Grid item xs={4} key={workout._id}>
+              <MuscleCard
+                name={workout.name}
+                image={workout.image}
+                onClick={goToSelectedWorkouts}
+              />
+            </Grid>
+          ))}
+        </Grid>
         <Button
-          onClick={goToSelectedWorkouts}
           sx={{ border: 3 }}
           color='secondary'
           variant='outlined'

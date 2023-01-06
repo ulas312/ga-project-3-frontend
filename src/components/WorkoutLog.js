5173645ea148f1430ee878b688339cedb33832d1
312 <a312@312s-MBP.broadband>
2023-01-06 09:33:32 +0000

    working on workout

--- src/components/WorkoutLog.js
+++ src/components/WorkoutLog.js
@@ -1,9 +1,51 @@
-const WorkoutLog = () => (
-  <section className='Home'>
-    <div className='welcome'>
-      <h1>WORKOUT LOG</h1>
+import * as React from 'react';
+import { DataGrid } from '@mui/x-data-grid';
+
+const columns = [
+  { field: 'id', headerName: 'ID', width: 70 },
+  { field: 'exercises', headerName: 'Exercises', width: 130 },
+  { field: 'lastName', headerName: 'Last name', width: 130 },
+  {
+    field: 'age',
+    headerName: 'Age',
+    type: 'number',
+    width: 90,
+  },
+  {
+    field: 'fullName',
+    headerName: 'Full name',
+    description: 'This column has a value getter and is not sortable.',
+    sortable: false,
+    width: 160,
+    valueGetter: (params) =>
+      `${params.row.firstName || ''} ${params.row.lastName || ''}`,
+  },
+];
+
+const rows = [
+  { id: 1, lastName: 'Snow', exercises: 'Jon', age: 35 },
+  { id: 2, lastName: 'Lannister', exercises: 'Cersei', age: 42 },
+  { id: 3, lastName: 'Lannister', exercises: 'Jaime', age: 45 },
+  { id: 4, lastName: 'Stark', exercises: 'Arya', age: 16 },
+  { id: 5, lastName: 'Targaryen', exercises: 'Daenerys', age: null },
+  { id: 6, lastName: 'Melisandre', exercises: null, age: 150 },
+  { id: 7, lastName: 'Clifford', exercises: 'Ferrara', age: 44 },
+  { id: 8, lastName: 'Frances', exercises: 'Rossini', age: 36 },
+  { id: 9, lastName: 'Roxie', exercises: 'Harvey', age: 65 },
+];
+
+const WorkoutLog = () => {
+  return (
+    <div style={{ height: 400, width: '100%' }}>
+      <DataGrid
+        rows={rows}
+        columns={columns}
+        pageSize={5}
+        rowsPerPageOptions={[5]}
+        // checkboxSelection
+      />
     </div>
-  </section>
-);
+  );
+};
 
 export default WorkoutLog;

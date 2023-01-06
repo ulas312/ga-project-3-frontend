1141be6ebb907db1cfe294e5a75a6993f55d822a
312 <a312@312s-MBP.broadband>
2023-01-06 10:32:06 +0000

    working on home and home.scss

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,20 +1,51 @@
 import * as React from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 import HomeImage from '../assets/home-background.png';
 import '../styles/Home.scss';
 import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
 
 const Home = () => (
   <Grid className='Home' container component='main' sx={{ height: '100vh' }}>
-    <img src={HomeImage} alt='man lifting weights' />
-    {/* <Box className='button' sx={{ '& button': { m: 7 } }}>
-      <div>
-        <Button variant='outlined' size='large'>
+    <Typography
+      component='div'
+      variant='body1'
+      style={{
+        height: 100,
+        width: '100%',
+        position: 'relative',
+      }}
+    >
+      <Box
+        sx={{
+          position: 'absolute',
+          top: 40,
+          left: '40%',
+          zIndex: 'tooltip',
+        }}
+      >
+        <Button
+          sx={{ border: 3 }}
+          color='secondary'
+          variant='outlined'
+          size='large'
+        >
           start Lifting
         </Button>
-      </div>
-    </Box> */}
+      </Box>
+      <Box
+        sx={{
+          position: 'absolute',
+          top: 0,
+          left: '43%',
+          zIndex: 'modal',
+        }}
+      >
+        <img src={HomeImage} alt='man lifting weights' />
+      </Box>
+    </Typography>
   </Grid>
 );
 

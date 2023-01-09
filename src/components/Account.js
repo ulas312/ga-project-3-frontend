d374ea4b5742dc01efdaae806f7f891bf2cc6e8e
ulas312 <temel.ulas@googlemail.com>
2023-01-09 10:34:43 +0000

    account page

--- src/components/Account.js
+++ src/components/Account.js
@@ -1,14 +1,58 @@
 import * as React from 'react';
-import Avatar from '@mui/material/Avatar';
-import Stack from '@mui/material/Stack';
+import AccountText from '../assets/account-text-black.png';
+import legs from '../assets/legs.png';
+import { LazyLoadImage } from 'react-lazy-load-image-component';
+// import 'react-lazy-load-image-component/src/effects/blur.css';
+
+import {
+  Container,
+  Box,
+  Card,
+  CardContent,
+  Button,
+  Typography,
+  Grid,
+  Stack,
+  Avatar,
+} from '@mui/material';
 
 const Account = () => (
-  <section className='Home'>
-    <h1>PROFILE PAGE</h1>
-    <Stack direction='row' spacing={2}>
-      <Avatar alt='Remy Sharp' src='/static/images/avatar/1.jpg' />
-    </Stack>
-  </section>
+  <>
+    <Box
+      component='img'
+      sx={{
+        position: 'absolute',
+        top: '8vh',
+        justify: 'center',
+        left: '40%',
+        // zIndex: 'tooltip',
+        mt: 4,
+        mb: 20,
+        height: 100,
+        width: 400,
+      }}
+      alt='Account page text'
+      src={AccountText}
+    />
+    <Grid
+      container
+      spacing={0}
+      direction='column'
+      alignItems='center'
+      justify='center'
+      style={{ minHeight: '100vh' }}
+    >
+      <Card>
+        {/* <LazyLoadImage alt={legs.alt} effect='blur' src={legs} /> */}
+        {/* <LazyLoadImage
+          alt={legs.alt}
+          height={legs.height}
+          src={legs} // use normal <img> attributes as props
+          width={legs.width}
+        /> */}
+      </Card>
+    </Grid>
+  </>
 );
 
 export default Account;
